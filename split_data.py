1066cc001ea283780e8bb62398c6af401fec4a6b
Jakub Adamek <kait@kaittop>
2014-04-06 12:43:49 +0200

    Data splitting script now also generates CV set

--- split_data.py
+++ split_data.py
@@ -1,13 +1,16 @@
 import random
 
-with open("issuelist", "r") as inp:
-    with open("datalist", "w") as outp:
-        with open("testlist", "w") as testp:
-            while True:
-                line = inp.readline()
-                if not line:
-                    break
-                if random.random() < 0.1:
-                    testp.write(line)
-                else:
-                    outp.write(line)
+with open("issue_list", "r") as inp:
+    with open("data_list", "w") as outp:
+        with open("cv_list", "w") as cvp:
+            with open("test_list", "w") as testp:
+                while True:
+                    line = inp.readline()
+                    if not line:
+                        break
+                    if random.random() < 0.05:
+                        testp.write(line)
+                    elif random.random() < 0.05:
+                        cvp.write(line)
+                    else:
+                        outp.write(line)
