1fd93893c8f9364d2232ce5958509d38d9198e2a
Jakub Adamek <kait@kaittop>
2014-05-21 13:55:10 +0200

    Refactored feeding code

--- script.py
+++ script.py
@@ -26,21 +26,24 @@ model_map = {
 }
 
 parser = argparse.ArgumentParser(description='Simple processing script')
-parser.add_argument('-data_file', type=str, help='Input file')
+parser.add_argument('-train_file', type=str, help='train file')
 parser.add_argument('-test_file', type=str, help='Test file')
 parser.add_argument('-num_bench', type=int, default=5, help='Number of benchmarks')
 parser.add_argument('-selected', type=int, default=2000, help='Selected by hash compressor')
 parser.add_argument('-model_file', type=str, help='File to read/write model to')
 parser.add_argument('-stats_file', type=str, help='File to read/write stats to')
 parser.add_argument('-generate', dest='generate', action='store_true', help='Should the model be generated')
+parser.add_argument('-evaluate_test', dest='evaluate_test', action='store_true', help='Should the test set be evaluated')
+parser.add_argument('-vectorize_train_output', type=str, help='Where should the vectorized train data be stored')
+parser.add_argument('-vectorize_test_output', type=str, help='Where should the vectorized test data be stored')
 parser.add_argument('-verbose', dest='verbose', action='store_true', help='Should we output compression details and model')
-parser.add_argument('-data_limit', type=int, default=None, help='Number of issues to use for training')
+parser.add_argument('-train_limit', type=int, default=None, help='Number of issues to use for training')
 parser.add_argument('-test_limit', type=int, default=None, help='Number of issues to use for testing')
 parser.add_argument('-do_stemming', dest='do_stemming', action='store_true', help='Should stemming be used')
 parser.add_argument('-do_dictionary', dest='do_dictionary', action='store_true', help='Should dictionary be used')
 parser.add_argument('-do_multi', dest='do_multi', action='store_true', help='Should multi be used')
 parser.add_argument('-model', dest='model', type=str, choices=model_map.keys(), help='Model to fit')
-parser.add_argument('-evaluate_on_data', dest='evaluate_on_data', action='store_true', help='Evaluate on data')
+parser.add_argument('-evaluate_on_train', dest='evaluate_on_train', action='store_true', help='Evaluate on train')
 
 logging.basicConfig(level=logging.INFO, filename="script_log", filemode="a+",
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -226,7 +229,7 @@ class Tester:
         self.stats_file = stats_file
         self.verbose = verbose
 
-    def feed(self, objs):
+    def consume(self, objs):
         logging.info("Evaluating %s test objects", len(objs))
 
         ft, expected, mt = zip(*objs)
@@ -283,38 +286,17 @@ class Tester:
             self.format_results(self.stats[ind])
             self.print("---------------------------------------------")
 
-def assert_stats_file_doesnt_exist(args):
-    if args.test_file or (args.data_file and args.evaluate_on_data):
+    def done(self):
+        self.print_stats()
+
+def check_args_consistent(args):
+    if args.test_file or (args.train_file and args.evaluate_on_train):
         if not args.stats_file:
             print("No file to store stats in")
             exit(1)
         if os.path.exists(args.stats_file):
             print("Stats file path exists")
             exit(1)
-
-def feed_tester(tester, processor, comp, test_file, test_limit=None):
-    lines_read = 0
-    test_objs = []
-    while True:
-        line = test_file.readline()
-        if not line or lines_read == test_limit:
-            tester.feed(test_objs)
-            break
-        lines_read += 1
-        obj = json.loads(line.strip())
-        proc_objs = processor.process_obj(obj)
-        comp_objs = comp.compress(proc_objs)
-        test_objs += comp_objs
-
-        if len(test_objs) >= 500:
-            tester.feed(test_objs)
-            test_objs = []
-
-def main():
-    args = parser.parse_args()
-
-    assert_stats_file_doesnt_exist(args)
-
     if args.generate:
         if not args.model_file:
             print("No file to store model in")
@@ -322,41 +304,118 @@ def main():
         if os.path.exists(args.model_file):
             print("Model file path exists")
             exit(1)
-        logging.info("Generating models")
 
+class Feeder:
+    def __init__(self, processor, comp, source_file, source_limit=None):
+        self.processor = processor
+        self.comp = comp
+        self.source_file = source_file
+        self.source_limit = source_limit
+
+    def feed(self, consumer):
+        lines_read = 0
+        test_objs = []
+        while True:
+            line = self.source_file.readline()
+            if not line or lines_read == self.source_limit:
+                consumer.consume(test_objs)
+                break
+            lines_read += 1
+            obj = json.loads(line.strip())
+            proc_objs = self.processor.process_obj(obj)
+            comp_objs = self.comp.compress(proc_objs)
+            test_objs += comp_objs
+
+            if len(test_objs) >= 500:
+                consumer.consume(test_objs)
+                test_objs = []
+
+class GenerationConsumer:
+    def __init__(self, gnbs, counts, model_file, processor, comp):
+        self.gnbs = gnbs
+        self.counts = counts
+        self.model_file = model_file
+        self.processor = processor
+        self.comp = comp
+
+    def consume(self, objs):
+        self.counts += feed(self.gnbs, objs)
+
+    def done(self):
+        with open(self.model_file, "w") as model_file:
+            pickle.dump(self.processor, model_file)
+            pickle.dump(self.comp, model_file)
+            pickle.dump(self.gnbs, model_file)
+            pickle.dump(self.counts, model_file)
+
+class OutputConsumer:
+    def __init__(self, output_file, selected):
+        self.output_file = output_file
+        self.output_file.write("@relation github-issues\n")
+        for i in range(selected):
+            self.output_file.write("@attribute %s numeric\n" % (i,))
+        self.output_file.write("@attribute output numeric\n")
+        self.output_file.write("@data\n")
+        self.selected = selected
+
+    def process_row(self, f, t):
+        self.output_file.write("{")
+        for i in xrange(self.selected):
+            if f[i] > 0.5:
+                self.output_file.write("%s %s," % (i, f[i]))
+        self.output_file.write("%s %s" % (self.selected, t))
+        self.output_file.write("}\n")
+
+    def consume(self, objs):
+        for f, t, m in objs:
+            self.process_row(f, t)
+
+    def done(self):
+        self.output_file.close()
+
+class MultiConsumer:
+    def __init__(self):
+        self.consumers = []
+
+    def add_consumer(self, consumer):
+        self.consumers.append(consumer)
+
+    def consume(self, objs):
+        for consumer in self.consumers:
+            consumer.consume(objs)
+
+    def done(self):
+        for consumer in self.consumers:
+            consumer.done()
+
+def main():
+    args = parser.parse_args()
+
+    check_args_consistent(args)
+
+    if args.generate or args.vectorize_train_output:
         processor_config = ProcessorConfiguration(do_dictionary=args.do_dictionary,
                 do_stemming=args.do_stemming, do_multi=args.do_multi)
 
         processor = Processor(processor_config)
         comp = HashCompressor(args.selected)
-        gnbs = [model_map[args.model]() for i in range(args.num_bench)]
-        counts = numpy.array([0 for i in range(args.num_bench)])
+        train_consumer = MultiConsumer()
+        if args.generate:
+            logging.info("Generating models")
+            gnbs = [model_map[args.model]() for i in range(args.num_bench)]
+            counts = numpy.array([0 for i in range(args.num_bench)])
+            train_consumer.add_consumer(GenerationConsumer(gnbs, counts, args.model_file, processor, comp))
+        if args.vectorize_train_output:
+            logging.info("Vectorizing train")
+            vectorize_train_output = open(args.vectorize_train_output, "w")
+            train_consumer.add_consumer(OutputConsumer(vectorize_train_output, args.selected))
         lines_read = 0
 
-        with open(args.data_file, "r") as inp:
-            todo_objs = []
-            while True:
-                line = inp.readline()
-                if not line or lines_read == args.data_limit:
-                    counts += feed(gnbs, todo_objs)
-                    break
-                lines_read += 1
-                obj = json.loads(line.strip())
-                proc_objs = processor.process_obj(obj)
-                comp_objs = comp.compress(proc_objs)
-                todo_objs += comp_objs
+        with open(args.train_file, "r") as inp:
+            Feeder(processor, comp, inp, args.train_limit).feed(train_consumer)
+        train_consumer.done()
 
-                if len(todo_objs) >= 500:
-                    counts += feed(gnbs, todo_objs)
-                    todo_objs = []
-
-        with open(args.model_file, "w") as model_file:
-            pickle.dump(processor, model_file)
-            pickle.dump(comp, model_file)
-            pickle.dump(gnbs, model_file)
-            pickle.dump(counts, model_file)
-
-    else:
+    elif not args.generate and args.model_file:
         logging.info("Loading models from file")
         with open(args.model_file, "r") as model_file:
             processor = pickle.load(model_file)
@@ -364,23 +423,31 @@ def main():
             comp = pickle.load(model_file)
             gnbs = pickle.load(model_file)
             counts = pickle.load(model_file)
-            gnb = gnbs[-1]
 
-    assert_stats_file_doesnt_exist(args)
     with open(args.stats_file, "w") as stats_file:
+        test_consumer = MultiConsumer()
+        if args.evaluate_test:
+            tester = Tester(gnbs, counts, comp.targets, stats_file, True)
+            logging.info("Evaluating test")
+            test_consumer.add_consumer(tester)
+        if args.vectorize_test_output:
+            logging.info("Vectorizing test")
+            vectorize_test_output = open(args.vectorize_test_output, "w")
+            test_consumer.add_consumer(OutputConsumer(vectorize_test_output, args.selected))
+
         if args.test_file:
             logging.info("Evaluating models on test data set")
             with open(args.test_file, "r") as test_file:
-                tester = Tester(gnbs, counts, comp.targets, stats_file, True)
-                feed_tester(tester, processor, comp, test_file, args.test_limit)
-                tester.print_stats()
+                Feeder(processor, comp, test_file, args.train_limit).feed(test_consumer)
+                test_consumer.done()
 
-        if args.data_file and args.evaluate_on_data:
-            logging.info("Evaluating models on train data set")
-            with open(args.data_file, "r") as data_file:
+        if args.train_file and args.evaluate_on_train:
+            logging.info("Evaluating models on train input set")
+            tester = Tester(gnbs, counts, comp.targets, stats_file, False)
+            with open(args.train_file, "r") as train_file:
                 tester = Tester(gnbs, counts, comp.targets, stats_file, False)
-                feed_tester(tester, processor, comp, data_file)
-                tester.print_stats()
+                Feeder(processor, comp, train_file, args.train_limit).feed(tester)
+                tester.done()
 
 if __name__ == "__main__":
     main()
