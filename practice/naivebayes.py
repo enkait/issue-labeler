1d0bc742aba93708f3fb8ea6e3c57ff64e073488
Jakub Adamek <kait@kaittop>
2014-06-29 21:54:26 +0200

    Remove weight calculation function as superfluous

--- practice/naivebayes.py
+++ practice/naivebayes.py
@@ -3,17 +3,6 @@ from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer, 
 from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 
-def calc_weights(Y):
-    Ya = np.array(Y)
-    res = np.array([0, 0, 0])
-    for i in range(3):
-        res[i] = sum(Ya == (i + 1))
-    res = res.astype(np.float64)
-    res = sum(res) / res
-    print res
-    W = map(lambda y: res[y-1], Y)
-    return W
-
 def main():
     vectorizer = CountVectorizer(stop_words="english",
             token_pattern=r"(?u)\b(?:\w|\?)(?:\w|\?)+\b", ngram_range=(1,2))
@@ -22,7 +11,6 @@ def main():
     Xt = vectorizer.fit_transform(X)
     Xcvt = vectorizer.transform(Xcv)
     model = MultinomialNB(fit_prior=False)
-    #model.fit(Xt, Y, calc_weights(Y))
     model.fit(Xt, Y)
     Pcv = model.predict(Xcvt)
     print_stats(Ycv, Pcv)
